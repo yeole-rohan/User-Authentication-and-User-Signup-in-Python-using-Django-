8f52d281a586b97b3c79f006e32fff4261cd1ad0
Harley Cruz <johnharleycruz592@gmail.com>
2025-10-02 16:01:56 +0800

    database/UI/validations

--- loginSignup/loginSignup/settings.py
+++ loginSignup/loginSignup/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'loginSignup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'qentry_db',
+        'USER': 'qentry_user',
+        'PASSWORD': 'StrongPassword123!',
+        'HOST': 'localhost',   # or your MySQL server IP
+        'PORT': '3306',
     }
 }
 
